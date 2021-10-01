bb6d81951496c7e00ab1bca4c692ed50a7a0ae1d
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-09-30 21:18:44 -0400

    App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export const App = () => {
+function App() {
+  const [count, setCount] = React.useState(0);
+  const [error, setError] = React.useState(false);
+
   return (
-    <div>
-      <div data-test='component-app' className='App'>
-        <h1 data-test='counter-display'>
-          The counter is currently&nbsp;
-          <span data-test='count'>{count}</span>
-        </h1>
-        {error && (
-          <p
-            data-test='error-message'
-            style={{ color: 'red', fontWeight: '600' }}
-          >
-            The counter cannot go below 0
-          </p>
-        )}
-        <button data-test='increment-button' onClick={incrementHandler}>
-          Increment counter
-        </button>
-        <button data-test='decrement-button' onClick={decrementHandler}>
-          Decrement counter
-        </button>
-      </div>{' '}
+    <div data-test='component-app' className='App'>
+      <h1 data-test='counter-display'>
+        The counter is currently&nbsp;
+        <span data-test='count'>{count}</span>
+      </h1>
+      {/* Notes: 
+      - using ternary on the error state to determine whether or not to hide 
+      - the 'error' and 'hidden' classes are defined in App.css
+      */}
+      <div
+        data-test='error-message'
+        className={`error ${error ? '' : 'hidden'}`}
+      >
+        The counter cannot go below 0
+      </div>
+      <button
+        data-test='increment-button'
+        onClick={() => {
+          if (error) {
+            setError(false);
+          }
+          setCount(count + 1);
+        }}
+      >
+        Increment counter
+      </button>
+      <button
+        data-test='decrement-button'
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          } else {
+            setError(true);
+          }
+        }}
+      >
+        Decrement counter
+      </button>
     </div>
   );
-};
+}
+
+export default App;
